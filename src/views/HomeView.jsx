40e0c42d5c72f86e6966a4a220e1401a33c75627
SongGayoung <xjongam1x@gmail.com>
2023-08-15 11:15:14 +0900

    header, navbar

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Skip from "../components/Skip";
-import Header from "../components/Header";
-import Main from "../components/Main";
+import Header from "../components/Header.jsx";
 import Intro from "../components/Intro";
 import Skill from "../components/Skill";
 import Site from "../components/Site";
 import Port from "../components/Port";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import Skip from "../components/Skip";
+import Main from "../components/Main";
 
 const HomeView = () => {
   return (
