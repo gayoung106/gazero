564b7cb954e3bc30b89917e87e3f4138c471efd2
SongGayoung <xjongam1x@gmail.com>
2023-08-15 16:28:24 +0900

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            <div>
+              <a
+                href="https://open.kakao.com/o/sPKynVof"
+                target="_blank"
+                rel="noreferrer"
+              >
+                KAKAO : Gazero(Frontend)
+              </a>
+            </div>
+            <div>
+              <a
+                href="mailto:soongin95@naver.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                mail : SOONGIN95@NAVER.COM
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
